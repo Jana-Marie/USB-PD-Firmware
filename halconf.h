edafb128a93003b6b1ef90edfd8c0208c16335cf
Clayton G. Hobbs <clay@lakeserv.net>
2017-03-06 20:32:57 -0500

    Added license headers
    The original, unmodified portions of ChibiOS retain their original
    copyright notices, for the portions of code to which they still apply.

--- halconf.h
+++ halconf.h
@@ -1,4 +1,22 @@
 /*
+ * PD Buddy - USB Power Delivery for everyone
+ * Copyright (C) 2017 Clayton G. Hobbs <clay@lakeserv.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
     ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +33,7 @@
 */
 
 /**
- * @file    templates/halconf.h
+ * @file    halconf.h
  * @brief   HAL configuration header.
  * @details HAL configuration file, this file allows to enable or disable the
  *          various device drivers from your application. You may also use
