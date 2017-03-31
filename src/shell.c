8d8b60142676ec9a21d652905234d189b798f89c
Clayton G. Hobbs <clay@lakeserv.net>
2017-03-31 12:57:44 -0400

    Added license command
    The command-line interface now shows license information if you type the
    "license" command.  Now users can be easily made aware of their
    freedoms.

--- src/shell.c
+++ src/shell.c
@@ -51,6 +51,33 @@ static struct pdb_config tmpcfg = {
     .status = PDB_CONFIG_STATUS_EMPTY
 };
 
+static void cmd_license(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    (void) argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: license\r\n");
+        return;
+    }
+
+    chprintf(chp,
+        "PD Buddy - USB Power Delivery for everyone\r\n"
+        "Copyright (C) 2017 Clayton G. Hobbs <clay@lakeserv.net>\r\n"
+        "\r\n"
+        "This program is free software: you can redistribute it and/or modify\r\n"
+        "it under the terms of the GNU General Public License as published by\r\n"
+        "the Free Software Foundation, either version 3 of the License, or\r\n"
+        "(at your option) any later version.\r\n"
+        "\r\n"
+        "This program is distributed in the hope that it will be useful,\r\n"
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of\r\n"
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\r\n"
+        "GNU General Public License for more details.\r\n"
+        "\r\n"
+        "You should have received a copy of the GNU General Public License\r\n"
+        "along with this program.  If not, see <http://www.gnu.org/licenses/>.\r\n"
+    );
+}
+
 static void cmd_erase(BaseSequentialStream *chp, int argc, char *argv[])
 {
     (void) argv;
@@ -192,6 +219,7 @@ static void cmd_identify(BaseSequentialStream *chp, int argc, char *argv[])
 }
 
 static const struct pdb_shell_cmd commands[] = {
+    {"license", cmd_license, "Show copyright and license information"},
     {"erase", cmd_erase, "Erase all stored configuration"},
     {"write", cmd_write, "Write the changes to flash"},
     {"load", cmd_load, "Load the stored configuration into RAM"},
